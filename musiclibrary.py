96683003f209072b7868256c47bc8dd43a41bfd9
AVI TYAGI <avityagi3205@gmail.com>
2026-08-20 17:44:00 +0530

    Add files via upload
    MUSIC LIBRARY PLAY A SONGS THAT WE WILL STORE

---
+++ musiclibrary.py
@@ -0,0 +1,5 @@
+music = {
+"stealth": "https://www.youtube.com/watch?v=U47Tr9BB_wE&list=RDU47Tr9BB_wE&start_radio=1",
+"saiyaara": "https://www.youtube.com/watch?v=BSJa1UytM8w&list=RDBSJa1UytM8w&start_radio=1",
+"roots": "https://www.youtube.com/watch?v=e36s_nZCD94&list=RDe36s_nZCD94&start_radio=1"
+}
